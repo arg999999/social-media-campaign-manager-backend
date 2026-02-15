dfb77e1fc57185d3689ddd4defa9cf789780ec0f
arg999999 <dev1.arg999@gmail.com>
2026-02-15 23:53:00 +0530

    page size changed

--- campaignpulse_backend-main/campaignpulse_backend/settings.py
+++ campaignpulse_backend-main/campaignpulse_backend/settings.py
@@ -134,5 +134,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.CursorPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 50,
 }
--- campaignpulse_backend-main/campaigns/pagination.py
+++ campaignpulse_backend-main/campaigns/pagination.py
@@ -1,5 +1,5 @@
 from rest_framework.pagination import CursorPagination
 
 class CampaignCursorPagination(CursorPagination):
-    page_size = 10
+    page_size = 50
     ordering = '-created_at'
